0d324d9d49820902ea60800104ec673bfbb1fce4
Andreas Hilbig <ahilbig@me.com>
2018-04-24 15:20:53 +0200

    Added threex.js type definitions

---
+++ types/threex/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for threex.js
+// Project: http://www.threejsgames.com/extensions/
+// Subprojects currently in scope: http://www.threejsgames.com/extensions/#threex.domevents
+// Definitions by: Andreas Hilbig <https://github.com/ahilbig>
+// Definitions: https://github.com/ahilbig/DefinitelyTyped
+
+export * from "./threex.domevents";
+export as namespace THREEx;
